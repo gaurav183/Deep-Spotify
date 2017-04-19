16473478ac021d369894f29a8054bc5869941a46
Gaurav Lahiry <glahiry@andrew.cmu.edu>
2017-04-18 23:27:12 -0400

    not all ids are valid

---
+++ neural_net.py
@@ -0,0 +1,144 @@
+import numpy as np
+import random
+import math
+
+
+# num hidden layers is 1
+class NeuralNet:
+  def __init__(self, structure, learning_rate):
+    """
+    Initialize the Neural Network.
+
+    - structure is a dictionary with the following keys defined:
+        num_inputs
+        num_outputs
+        num_hidden
+    - learning rate is a float that should be used as the learning
+        rate coefficient in training
+
+    When building your net, make sure to initialize your weights
+    to random values in the range [-0.05, 0.05]. Specifically, you
+    should use some transformation of 'np.random.rand(n,m).'
+    """
+
+    self.num_inputs = structure['num_inputs']
+    self.num_outputs = structure['num_outputs']
+    self.num_hidden = structure['num_hidden']
+    self.learning_rate = learning_rate
+
+    self.x = []
+
+    self.w1 =(np.random.rand(self.num_inputs + 1, self.num_hidden) - 0.5)/10.0
+
+    self.w2 = (np.random.rand(self.num_hidden, self.num_outputs) - 0.5)/10.0
+
+
+
+  def apply_sigmoid(self, x):
+    return float(1.0/float((1.0+math.e**(float(-1*x)))))
+
+  def get_weights(self):
+    """
+    Returns (w1, w2) where w1 is a matrix representing the current
+    weights from the input to the hidden layer and w2 is a similar
+    matrix for the hidden to output layers. Specifically, w1[i,j]
+    should be the weight from input node i to hidden unit j.
+    """
+
+    return (self.w1, self.w2)
+
+  def forward_propagate(self, x):
+    """
+    Push the input 'x' through the network and returns the activations
+    on the output nodes.
+
+    - x is a numpy array representing an input to the NN
+
+    Return a numpy array representing the activations of the output nodes.
+
+    Hint: you may want to update state here, since you should call this
+    method followed by back_propagate in your train method.
+    """
+
+    # add 1 for "bias"
+    x = np.append(x, [1])
+    self.x = x
+    (w1, w2) = (self.w1, self.w2)
+    product = np.dot([x], w1)
+    for i in xrange(self.num_hidden):
+        product[0][i] = self.apply_sigmoid(product[0][i])
+    self.sigmoid_product = product 
+    predicted=np.dot(product, w2)
+    for i in xrange(self.num_outputs):
+        predicted[0][i] = self.apply_sigmoid(predicted[0][i])
+    self.predicted = predicted
+    return predicted[0]
+
+
+  def back_propagate(self, target):
+    """
+    Updates the weights of the NN for the last forward_propagate call.
+
+    - target is the label of the last forward_propogate input
+    """
+
+    (w1, w2) = (self.w1, self.w2)
+    predicted = self.predicted
+    sigmoid_product = self.sigmoid_product
+    delt2 = [float(0) for i in range(self.num_outputs)]
+    for i in xrange(self.num_outputs):
+        delt2[i] = float(predicted[0][i]*(1-predicted[0][i])*(target[i]-predicted[0][i]))
+    temp  = np.multiply(w2, delt2)
+    sum_temp= np.sum(temp, axis=1)
+    delth=[[float(0)] for i in range(self.num_hidden)]
+    for i in xrange(self.num_hidden):
+        delth[i][0]=float(sigmoid_product[0][i]*(float(1)-sigmoid_product[0][i])*sum_temp[i])
+
+    delt2_scaled = np.multiply(delt2, self.learning_rate)
+    deriv_w2 = np.multiply(np.transpose(sigmoid_product), delt2_scaled) 
+    delt1 = np.transpose(np.dot(delth, [self.x]))
+    deriv_w1 = np.multiply(delt1, self.learning_rate)
+    self.w2 =  np.add(w2, deriv_w2)
+    self.w1 = np.add(w1, deriv_w1)
+
+
+  def train(self, X, Y, iterations=100):
+    """
+    Trains the NN on observations X with labels Y.
+
+    - X is a numpy matrix (array of arrays) corresponding to a series of
+        observations. Each row is a new observation.
+    - Y is a numpy matrix (array of arrays) corresponding to the labels
+        of the observations.
+    - iterations is how many passes over X should be completed.
+    """
+    for i in xrange(iterations):
+        for j in xrange((X.shape)[0]):
+            self.forward_propagate(X[j])
+            label = np.zeros(shape=(10,), dtype=np.float32)
+            label[Y[j]] = float(1)
+            self.back_propagate(label)
+
+  def test(self, X, Y):
+    """
+    Tests the NN on observations X with labels Y.
+
+    - X is a numpy matrix (array of arrays) corresponding to a series of
+        observations. Each row is a new observation.
+    - Y is a numpy matrix (array of arrays) corresponding to the labels
+        of the observations.
+
+    Returns the mean squared error.
+    """
+
+    wrong = float(0)
+    for i in xrange((X.shape)[0]):
+        self.forward_propagate(X[i])
+        prob = np.argmax(self.predicted[0])
+        if prob != (Y[i]):
+            wrong+=1
+            
+    return float(wrong)/float((Y.shape)[0])
+   
+
+
---
+++ test.py
@@ -0,0 +1,126 @@
+import spotipy 
+
+import sys
+import json
+import os
+import spotipy.util as util
+import csv
+import numpy as np
+
+from neural_net import NeuralNet
+
+
+# export SPOTIPY_CLIENT_ID='3da3c80782214b98bda0a858bccaec0e'
+# export SPOTIPY_CLIENT_SECRET='250f2c4858da43029bf11162907f5f5e'
+# export SPOTIPY_REDIRECT_URI='https://www.google.com'
+
+# OAUTH TOKEN = BQCMmat8Cy3Bpfe7iuxo36m8jcTQagDxgtx0tLg6wycOH9SRDn2I8456vXpDinJWtOZ0Jnf7p0-Fo6RrgqVv0pdei91F9ESZVa-LEMvG1KgR_rkp2seMCGAOarfocur23WQ_mv9raJ0
+
+def get_track_ids():
+    track_ids = []
+    # top 200 songs from spotify charts
+    with open('regional-global-daily-latest.csv', 'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            url = row['URL']
+            url_list = url.split("/")
+            track_ids.append(url_list[-1])
+  
+    # one million songs from echonest song dataset 
+    subdirectories = os.walk("millionsongdataset_echonest")
+    for roots, dirs, files in subdirectories:
+        folder = os.walk(roots)
+        for r, d, filenames in folder:
+            if (r != "millionsongdataset_echonest"):
+                for filename in filenames:
+                    if (filename[0] != "."):
+                        with open(os.path.join(r, filename)) as data_file:  
+                            data = json.load(data_file)
+
+                        response = data["response"]
+                        songs = response["songs"]
+                        if (len(songs) > 0):
+                            # database website says only one song per file
+                            tracks = songs[0]["tracks"]
+                            spotify_found = False
+                            for track in tracks:
+                                if (track["catalog"] == "spotify"):
+                                    foreign_id = (track["foreign_id"]).split(":")
+                                    track_ids.append(foreign_id[-1])
+                                    spotify_found = True
+                                    break
+
+    return track_ids
+
+username = '1259307384'
+token = util.prompt_for_user_token(username)
+if (token):
+    sp = spotipy.Spotify(token)
+
+    # get ids (list of song ids)
+    
+    track_ids = get_track_ids()
+    # print len(track_ids)
+
+    analysis = []
+    genres = []
+
+    for i in xrange(0, len(track_ids), 50):
+        num = min(50,len(track_ids)-i)
+        id_section = track_ids[i:i+num]
+
+        tracks = sp.tracks(id_section)['tracks']
+
+        artist_ids = []
+        for track in tracks:
+            artist_ids.append(track['artists'][0]['uri'])
+
+        artists = sp.artists(artist_ids)['artists']
+
+        count = 0
+        for artist in artists:
+            artist_genre = artist['genres']
+            if (len(artist_genre)==0):
+                #assert(len(sp.artist((sp.track(id_section[count]))['artists'][0]['uri'])['genres'])==0)
+                del id_section[count]
+                count -= 1
+            else:
+                genres.append(artist_genre)
+
+            count += 1
+
+        analysis.extend(sp.audio_features(tracks=id_section))
+
+    # print len(genres)
+    # print len(analysis)
+
+
+    input_features = np.empty([len(analysis), 9])
+    row = 0
+
+    for analyzed in analysis:
+        feature_list = [analyzed['energy'], analyzed['liveness'], analyzed['tempo'], 
+                        analyzed['speechiness'], analyzed['acousticness'], 
+                        analyzed['instrumentalness'], analyzed['danceability'],
+                        analyzed['loudness'], analyzed['valence']]
+
+        input_features[row] = feature_list
+        row += 1
+
+    # call NN with input_features
+    # learning_rate = 0.5
+    # structure = {'num_inputs': 9, 'num_hidden': 5, 'num_outputs': 10}
+    # candidate = NeuralNet(structure, learning_rate)
+
+    # #iterations = 15000
+    # iterations = 20
+
+    # candidate.train(input_features, labels, iterations)
+
+    # cand_error = candidate.test(input_features, labels)
+    # print "Train fraction: ", cand_error
+          
+else:
+    print("Can't get token for", username)
+
+    
