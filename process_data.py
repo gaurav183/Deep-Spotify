13ed7c13266c32089f0072caf1f6cf79e5c72edd
abhyvyth <abhy@Abhys-MBP.wv.cc.cmu.edu>
2017-04-25 18:52:21 -0400

    updated process data file to include more top spotify playlists

--- process_data.py
+++ process_data.py
@@ -6,51 +6,38 @@ import os
 import spotipy.util as util
 import csv
 import numpy as np
+import operator
 
-from neural_net import NeuralNet
+#from neural_net import NeuralNet
 
 
 # export SPOTIPY_CLIENT_ID='3da3c80782214b98bda0a858bccaec0e'
 # export SPOTIPY_CLIENT_SECRET='250f2c4858da43029bf11162907f5f5e'
 # export SPOTIPY_REDIRECT_URI='https://www.google.com'
 
-# TOKEN_H = "BQCMmat8Cy3Bpfe7iuxo36m8jcTQagDxgtx0tLg6wycOH9SRDn2I8456vXpDinJWtOZ0Jnf7p0-Fo6RrgqVv0pdei91F9ESZVa-LEMvG1KgR_rkp2seMCGAOarfocur23WQ_mv9raJ0"
+OAUTH_TOKEN = 'BQCMjTEnwkwv5gtWm_0DFLi6HgmVGYWPt-rdWvgCxtWGvmv9zzQXgO4LOdfqQ-moWkWiiOIvnXhfloVKrKrybDp5myCqbRC5ljmjGTSXaPm2JR8yA8TD27_MljhubF5FEaKCacd2FXY'
+def get_spotify_artist_ids():
+    artist_ids = []
+    categories = sp.categories()["categories"]
+    items = categories["items"]
+    for item in items[:20]:
+        cat_id = item["id"]
+        playlists = sp.category_playlists(cat_id)["playlists"]
+        for playlist in playlists["items"][:15]:
+            user = playlist["owner"]["id"]
+            playlist_id = (playlist["uri"].split(":"))[-1]
+            user_playlist = sp.user_playlist_tracks(user, playlist_id)
+            tracks = user_playlist["items"]
+            for track in tracks:
+                try:
+                    artists = track["track"]["artists"]
+                    artist_ids.append((artists[0]["uri"].split(":"))[-1])
+                except:
+                    pass
+    return artist_ids
 
-def get_track_ids():
-    track_ids = []
-    # top 200 songs from spotify charts
-    with open('regional-global-daily-latest.csv', 'rb') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            url = row['URL']
-            url_list = url.split("/")
-            track_ids.append(url_list[-1])
-  
-    # one million songs from echonest song dataset 
-    subdirectories = os.walk("millionsongdataset_echonest")
-    for roots, dirs, files in subdirectories:
-        folder = os.walk(roots)
-        for r, d, filenames in folder:
-            if (r != "millionsongdataset_echonest"):
-                for filename in filenames:
-                    if (filename[0] != "."):
-                        with open(os.path.join(r, filename)) as data_file:  
-                            data = json.load(data_file)
 
-                        response = data["response"]
-                        songs = response["songs"]
-                        if (len(songs) > 0):
-                            # database website says only one song per file
-                            tracks = songs[0]["tracks"]
-                            spotify_found = False
-                            for track in tracks:
-                                if (track["catalog"] == "spotify"):
-                                    foreign_id = (track["foreign_id"]).split(":")
-                                    track_ids.append(foreign_id[-1])
-                                    spotify_found = True
-                                    break
 
-    return track_ids
 
 def read_track_ids():
     with open('song_ids.txt') as idsFile:
@@ -58,22 +45,56 @@ def read_track_ids():
 
     return track_ids
 
+
 username = '12180915492'
+genre_dict = {}
 token = util.prompt_for_user_token(username)
 if (token):
     sp = spotipy.Spotify(token)
 
     # get ids (list of song ids)
     
-    # track_ids = get_track_ids()
 
     track_ids = read_track_ids();
-    print len(track_ids)
+
 
     analysis = []
     genres = []
 
-    numPos = 500#len(track_ids)
+    # validIds = []
+    temp_artist_ids = get_spotify_artist_ids()
+    
+    for i in xrange(0, len(temp_artist_ids), 50):
+        num = min(50,len(temp_artist_ids)-i)
+        id_section = temp_artist_ids[i:i+num]
+
+        try:
+            artists = sp.artists(id_section)['artists']
+        except:
+            token = util.prompt_for_user_token(username)
+            sp = spotipy.Spotify(token)
+            artists = sp.artists(id_section)['artists']
+
+        count = 0
+        for artist in artists:
+            artist_genre = artist['genres']
+            if (len(artist_genre)==0):
+                #assert(len(sp.artist((sp.track(id_section[count]))['artists'][0]['uri'])['genres'])==0)
+                del id_section[count]
+                count -= 1
+            else:
+                for genre in artist_genre:
+                    if genre in genre_dict:
+                        genre_dict[genre] +=1
+                    else:
+                        genre_dict[genre] = 1
+                genres.append(artist_genre)
+
+            count += 1
+
+        analysis.extend(sp.audio_features(tracks=id_section))
+
+    numPos = 1000
     for i in xrange(0, numPos, 50):
         num = min(50,numPos-i)
         id_section = track_ids[i:i+num]
@@ -85,24 +106,23 @@ if (token):
             sp = spotipy.Spotify(token)
             tracks = sp.tracks(id_section)['tracks']
 
+
         # numNones = tracks.count(None)
         # for num in xrange(numNones):
         #     index = tracks.index(None)
-        #     print id_section[index]
         #     del id_section[index]
-            # del tracks[index]
+        #     del tracks[index]
 
-        
-        # print "writing ", i
-        # for id in id_section:
-        #     thefile.write("%s\n" % id)
+        # validIds.extend(id_section)
 
         artist_ids = []
         for track in tracks:
             artist_ids.append(track['artists'][0]['uri'])
                 
-
         artists = sp.artists(artist_ids)['artists']
+
+
+
 
         count = 0
         for artist in artists:
@@ -112,15 +132,24 @@ if (token):
                 del id_section[count]
                 count -= 1
             else:
+                for genre in artist_genre:
+                    if genre in genre_dict:
+                        genre_dict[genre] +=1
+                    else:
+                        genre_dict[genre] = 1
                 genres.append(artist_genre)
 
             count += 1
 
         analysis.extend(sp.audio_features(tracks=id_section))
 
+    # with open('song_ids.txt') as thefile:
+    #     print "writing"
+    #     for id in validIds:
+    #         thefile.write("%s\n" % id)
 
-    print len(genres)
-    print len(analysis)
+    sorted_x = sorted(genre_dict.items(), key=operator.itemgetter(1))
+    print sorted_x
 
 
     # input_features = np.empty([len(analysis), 9])
